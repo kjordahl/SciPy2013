25c77d79f759e0f5a1d36b0fd5d3f57ef30699a4
Kelsey Jordahl <kjordahl@enthought.com>
2013-06-19 18:08:04 -0400

    Update check_install script for clarity and color if available

--- check_install.py
+++ check_install.py
@@ -1,37 +1,59 @@
+"""
+Test import of required and optional packages for SciPy 2013 tutorial
+Using Geospatial Data With Python
+
+"""
+
+try:
+    import curses
+    curses.setupterm()
+    assert curses.tigetnum("colors") > 2
+    OK = "\x1b[1;%dm[ OK ]\x1b[0m" % (30 + curses.COLOR_GREEN)
+    FAIL = "\x1b[1;%dm[FAIL]\x1b[0m" % (30 + curses.COLOR_RED)
+except:
+    OK = '[ OK ]'
+    FAIL = '[FAIL]'
+
+print
+print 'Required packages:'
+
 try:
     import shapely
-    print 'Shapely version', shapely.__version__
+    print OK, 'Shapely version', shapely.__version__
 except ImportError:
-    print 'Shapely not installed'
+    print FAIL, 'Shapely not installed'
 
 try:
     import fiona
-    print 'Fiona version', fiona.__version__
+    print OK, 'Fiona version', fiona.__version__
 except ImportError:
-    print 'Fiona not installed'
+    print FAIL, 'Fiona not installed'
 
 try:
     import pyproj
-    print 'pyproj version', pyproj.__version__
+    print OK, 'pyproj version', pyproj.__version__
 except ImportError:
-    print 'pyproj not installed'
+    print FAIL, 'pyproj not installed'
 
 try:
     import osgeo.gdal
-    print 'GDAL version', osgeo.gdal.__version__
+    print OK, 'GDAL version', osgeo.gdal.__version__
 except:
-    print 'GDAL not installed'
+    print FAIL, 'GDAL not installed'
 
 try:
     from mpl_toolkits.basemap import __version__
-    print 'Basemap version', __version__
+    print OK, 'Basemap version', __version__
 except ImportError:
-    print 'Basemap not installed'
+    print FAIL, 'Basemap not installed'
+
+print
+print 'Optional packages:'
 
 try:
     import geojson
 except ImportError:
-    print 'geoJSON not installed'
+    print FAIL, 'geoJSON not installed'
 else:
-    print 'geoJSON is installed'
+    print OK, 'geoJSON is installed'
 
